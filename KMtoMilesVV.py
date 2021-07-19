f7d08eba54f6ba6299c6ab13af647b9fbc9c3001
DLRDEV <72132610+Albinotaser@users.noreply.github.com>
2021-07-19 11:43:43 +0200

    Add files via upload

---
+++ KMtoMilesVV.py
@@ -0,0 +1,21 @@
+#The formule to convert KM to MILES = Amount of KM * 0.62
+
+user_input = float(input('How many KM do you wanna convert to miles? '))
+
+KM_result = user_input * 0.62
+
+KM_results = print(str(user_input) + "KM" + " is " + str(KM_result) + " miles")
+
+
+#If you wanna convert MILES to KM you simply use this formule = MILES * 1.6
+
+second_input = float(input("How many miles do you wanna convert into KM? "))
+
+MILE_result = second_input * 1.6
+
+MILES_results = print(str(second_input) + "MILES" + " is " + str(MILE_result) + " in KM")
+
+#Closing of the script and some referals
+print("\n\nI hope you enjoyed using our function to get a result that you hoped for!")
+print("\nYou can get in contact with the creator at this Discord tag = Hypaspist#9730")
+
